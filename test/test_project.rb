e288775698f778662131678802820ddc530daa45
Rafael George <george.rafael@gmail.com>
2011-05-30 18:26:21 -0400

    remove exception test for initialization process

--- test/test_project.rb
+++ test/test_project.rb
@@ -1,10 +1,13 @@
 require 'helper'
 
 class TestProject < Test::Unit::TestCase
-  
-  should "be able to load all projects" do 
+
+  def setup
     @klass = TicketMaster::Provider::Fogbugz::Project
     @tm = TicketMaster.new(:fogbugz, :email => 'email@test.com', :password => '12345', :uri => 'http://fogbugz.testing.com')
+  end
+  
+  should "be able to load all projects" do 
     @tm.projects.should be_an_instance_of(Array)
     @tm.projects.first.should be_an_instance_of(@klass)
   end
--- test/test_ticketmaster-fogbugz.rb
+++ test/test_ticketmaster-fogbugz.rb
@@ -7,9 +7,5 @@ class TestTicketmasterFogbugz < Test::Unit::TestCase
       assert_equal true, @fogbugz.instance_of?(TicketMaster)
       assert_equal true, @fogbugz.kind_of?(TicketMaster::Provider::Fogbugz)
     end
-
-    should "throw an error message in case any require field is missing" do 
-      lambda { TicketMaster.new(:fogbugz, :password => '12345', :uri => 'http://fogbugz.testing.com') }.should raise_error
-    end
   end
 end
