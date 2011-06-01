f9456cfd0dd136bb3ddfbc301d1b6e9982efc43f
Rafael George <george.rafael@gmail.com>
2011-06-01 17:59:23 -0400

    initial integration test for projects

--- test/test_project.rb
+++ test/test_project.rb
@@ -1,16 +1,15 @@
 require 'helper'
 
 class TestProject < Test::Unit::TestCase
+  CREDENTIALS = {:email => 'rafael@hybridgroup.com', :password => '123456', :uri => 'https://clutchapp.fogbugz.com'}
 
   def setup
     @klass = TicketMaster::Provider::Fogbugz::Project
-    @tm = TicketMaster.new(:fogbugz, :email => 'email@test.com', :password => '12345', :uri => 'http://fogbugz.testing.com')
-    project = {:id => 1}
-    ::Fogbugz::Interface.expects(:command).with(:listProjects).returns([project])
+    @tm = TicketMaster.new(:fogbugz, CREDENTIALS)
   end
   
   should "be able to load all projects" do 
-    @tm.projects.should be_an_instance_of(Array)
-    @tm.projects.first.should be_an_instance_of(@klass)
+    assert_equal true, @tm.projects.instance_of?(Array)
+    assert_equal true, @tm.projects.first.instance_of?(@klass)
   end
 end
--- test/test_ticketmaster-fogbugz.rb
+++ test/test_ticketmaster-fogbugz.rb
@@ -3,7 +3,7 @@ require 'helper'
 class TestTicketmasterFogbugz < Test::Unit::TestCase
   context "checking for ticketmaster initialization" do 
     should "be able to instatiate a new instance" do 
-      @fogbugz = TicketMaster.new(:fogbugz, :email => 'fog@email.com', :password => '12345', :uri => 'http://fogbugz.testing.com')
+      @fogbugz = TicketMaster.new(:fogbugz, :email => 'rafael@hybridgroup.com', :password => '12345', :uri => 'https://clutchapp.fogbugz.com')
       assert_equal true, @fogbugz.instance_of?(TicketMaster)
       assert_equal true, @fogbugz.kind_of?(TicketMaster::Provider::Fogbugz)
     end
