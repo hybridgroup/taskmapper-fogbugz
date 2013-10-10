aa18ab2db9f7cfdb35ab71ef32c13109f48b1cfc
Andrew Stewart <andrew@stwrt.ca>
2013-10-10 15:52:50 -0700

    Clean up Gemfile and put version.rb in correct place

--- lib/taskmapper-fogbugz.rb
+++ lib/taskmapper-fogbugz.rb
@@ -1,6 +1,6 @@
 #require YOUR_PROVIDER_API
 require 'fogbugz'
 
-%w{ fogbugz ticket project comment }.each do |f|
+%w{ fogbugz ticket project comment version }.each do |f|
   require File.dirname(__FILE__) + '/provider/' + f + '.rb';
 end
